6d64b7cd1e4353a92a3d5113f2c51693689db78c
Sam Vitello <sam.j.vitello@gmail.com>
2017-10-13 11:37:28 -0700

    evidence components

--- src/DisputeResolution/Evidence/index.js
+++ src/DisputeResolution/Evidence/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import DropdownButton from './DropdownButton'
+import EvidenceRow from './EvidenceRow'
 import './Evidence.css'
 
 const Evidence = (props) => {
@@ -13,6 +14,12 @@ const Evidence = (props) => {
         </div>
       </div>
       <div className="divider"></div>
+      { props.evidence.map(document => (
+        <div>
+          <EvidenceRow key={ document.name } name={ document.name } description={ document.description } />
+          <div className="divider"></div>
+        </div>
+      ))}
     </div>
   )
 }
