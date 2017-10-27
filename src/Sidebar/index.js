2f711fe8995b927a8cfcd4635019c2b8ff3c43de
Sam Vitello <sam.j.vitello@gmail.com>
2017-10-27 15:17:26 -0700

    buy pinakions in Settings

--- src/Sidebar/index.js
+++ src/Sidebar/index.js
@@ -1,23 +1,55 @@
-import React from 'react'
+import React, { Component } from 'react'
 import ShortProfile from './ShortProfile'
 import MenuSidebar from './MenuSidebar'
+import { connect } from 'react-redux'
+import { withRouter } from 'react-router-dom'
+import { balanceFetchData, fetchAddress } from '../business/ethereum/action-creators'
 import './Sidebar.css'
 
-const Sidebar = ({
-  className,
-  address,
-  balancePNK = 0,
-  items = [],
-  children,
-  ...rest
-}) => (
-  <div className={`Sidebar-container ${className}`}>
-    <ShortProfile address={address} balancePNK={balancePNK} />
-    <div className='divider' />
-    <MenuSidebar items={items} />
-    <div className='emptybar' />
-    { children }
-  </div>
-)
+class Sidebar extends Component {
+  componentWillMount () {
+    this.props.getBalance()
+    this.props.getAddress()
+  }
 
-export default Sidebar
+  render () {
+    let balance = 0
+    let address = 'loading...'
+
+    if (!this.props.balanceIsFetching) balance = this.props.balance
+    if (!this.props.addressIsFetching) address = this.props.address
+
+    if (this.props.balanceHasErrored) balance = -1
+    if (this.props.addressHasErrored) balance = '-error-'
+
+    return (
+      <div className={`Sidebar-container ${this.props.className}`}>
+        <ShortProfile address={address} balancePNK={balance} />
+        <div className='divider' />
+        <MenuSidebar items={this.props.items} />
+        <div className='emptybar' />
+        { this.props.children }
+      </div>
+    )
+  }
+}
+
+const mapStateToProps = state => {
+  return {
+    balance: state.ethereum.balance,
+    balanceHasErrored: state.ethereum.failureBalance,
+    balanceIsFetching: state.ethereum.requestBalance,
+    address: state.ethereum.address,
+    addressHasErrored: state.ethereum.failureAddress,
+    addressIsFetching: state.ethereum.requestAddress
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    getBalance: url => dispatch(balanceFetchData()),
+    getAddress: url => dispatch(fetchAddress())
+  }
+}
+
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(Sidebar))
--- src/bootstrap/reducers.js
+++ src/bootstrap/reducers.js
@@ -1,11 +1,11 @@
 import { combineReducers } from 'redux'
 import { reducer as formReducer } from 'redux-form'
-import balance from '../business/ethereum'
+import ethereum from '../business/ethereum'
 import disputes from '../business/disputes'
 import contract from '../business/contract'
 
 export default combineReducers({
-  balance,
+  ethereum,
   disputes,
   contract,
   form: formReducer
