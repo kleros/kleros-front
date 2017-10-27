98031ba71c39f04e1e0a71320a12981fd77bc4ff
Sam Vitello <sam.j.vitello@gmail.com>
2017-10-27 15:22:04 -0700

    settings

---
+++ src/Settings/BuyPinakion/index.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import BuyPinakionForm from './Form'
+
+class BuyPinakion extends Component {
+  render () {
+    return (
+      <div className='BuyPinakion'>
+        <div className='description'>
+          <h1>Buy Pinakion</h1>
+          <p>1 ETH = 1 PNK</p>
+        </div>
+        <BuyPinakionForm />
+      </div>
+    )
+  }
+}
+
+export default BuyPinakion
