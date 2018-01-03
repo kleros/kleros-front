cdc8c55698cbe4c5cebd2f194fc5b570074e1c1a
z <ahmedzubair216@gmail.com>
2018-01-03 20:36:26 +0530

    added account locked sidebar to storybook's sidebar

--- src/stories/index.js
+++ src/stories/index.js
@@ -23,7 +23,7 @@ import '../bootstrap/index.css'
 /** *************** Icon *****************/
 
 storiesOf('Icon', module).add('Icon with name=Disputes, theme=dark', () => (
-  <Icon name="Disputes" theme="dark" />
+  <Icon name='Disputes' theme='dark' />
 ))
 
 /** *************** Identicon *****************/
@@ -40,15 +40,15 @@ storiesOf('ShortProfile', module)
   .add('default', () => <ShortProfile />)
   .add('with icon, username and balance', () => (
     <ShortProfile
-      address="0x5DF9B87991262F6BA471F09758CDE1c0FC1De734"
+      address='0x5DF9B87991262F6BA471F09758CDE1c0FC1De734'
       balancePNK={242}
       icon={<Identicon />}
     />
   ))
   .add('with dark background, icon, address and balance', () => (
     <ShortProfile
-      theme="dark"
-      address="0x5DF9B87991262F6BA471F09758CDE1c0FC1De734"
+      theme='dark'
+      address='0x5DF9B87991262F6BA471F09758CDE1c0FC1De734'
       balancePNK={242}
       icon={<Identicon />}
     />
@@ -57,13 +57,13 @@ storiesOf('ShortProfile', module)
     'with dark background, icon, address, balance and active notification',
     () => (
       <ShortProfile
-        theme="dark"
-        address="0x5DF9B87991262F6BA471F09758CDE1c0FC1De734"
+        theme='dark'
+        address='0x5DF9B87991262F6BA471F09758CDE1c0FC1De734'
         balancePNK={242}
         notificationIsActive
         icon={<Identicon />}
       />
-    ),
+    )
   )
 
 /** *************** ItemMenuSidebar *****************/
@@ -72,7 +72,7 @@ storiesOf('ItemMenuSidebar', module)
   .addDecorator(StoryRouter())
   .add('default', () => <ItemMenuSidebar />)
   .add('with dark background and name', () => (
-    <ItemMenuSidebar name="items" theme="dark" />
+    <ItemMenuSidebar name='items' theme='dark' />
   ))
 
 /** *************** MenuSidebar *****************/
@@ -93,11 +93,12 @@ storiesOf('Sidebar', module)
   ))
   .add('with items=Disputes, Contracts, Jury', () => (
     <Sidebar
-      address="0xa1e4380a3b1f749673e270229993ee55f35663b4"
+      address='0xa1e4380a3b1f749673e270229993ee55f35663b4'
       balancePNK={242}
       items={['Disputes', 'Contracts', 'Jury']}
     />
   ))
+  .add('with account locked', () => <Sidebar />)
 
 /** *************** SearchBar *****************/
 
@@ -108,14 +109,14 @@ storiesOf('SearchBar', module).add('default', () => <SearchBar />)
 storiesOf('Input', module)
   .add('default', () => <Input />)
   .add('input text type with placeholder', () => (
-    <Input type="text" placeholder="Add your name here" />
+    <Input type='text' placeholder='Add your name here' />
   ))
   .add('textarea with placeholder', () => (
-    <Input type="textarea" placeholder="Add your comments" />
+    <Input type='textarea' placeholder='Add your comments' />
   ))
-  .add('radio button', () => <Input type="radio" />)
+  .add('radio button', () => <Input type='radio' />)
   .add('file input type with placeholder', () => (
-    <Input type="file" placeholder="Browse file" />
+    <Input type='file' placeholder='Browse file' />
   ))
 
 /** *************** TruncatedTextBox *****************/
@@ -124,13 +125,13 @@ storiesOf('TruncatedTextBox', module)
   .add('default', () => <TruncatedTextBox />)
   .add('with text and truncatedCharacters=4', () => (
     <TruncatedTextBox
-      text="with text length is greater than/equal to truncatedCharacters"
+      text='with text length is greater than/equal to truncatedCharacters'
       truncatedCharacters={4}
     />
   ))
   .add('with text length less than truncatedCharacter=8', () => (
     <TruncatedTextBox
-      text="with text length is less than truncatedCharacters"
+      text='with text length is less than truncatedCharacters'
       truncatedCharacters={8}
     />
   ))
@@ -141,7 +142,7 @@ storiesOf('Grid', module)
   .add('default', () => <Grid />)
   .add('with input text and icon', () => (
     <Grid>
-      <Input type="text" placeholder="Type something here" />
-      <Icon name="Disputes" theme="dark" />
+      <Input type='text' placeholder='Type something here' />
+      <Icon name='Disputes' theme='dark' />
     </Grid>
   ))
